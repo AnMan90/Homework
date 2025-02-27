bb75889718f87c00f1a8534d7588c139fd5ba2e6
AndreyM <kalisnrat@gmail.com>
2025-02-27 20:26:59 +0300

    add get date

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-from .masks import get_mask_card_number, get_mask_account
+from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card: str) -> str:
@@ -7,3 +7,8 @@ def mask_account_card(account_card: str) -> str:
         return f"{account_card[:4]} {get_mask_account(int(account_card[5:]))} "
     else:
         return f"{account_card[:-17]} {get_mask_card_number(int(account_card[-16:]))}"
+
+
+def get_date(date: str) -> str:
+    """Функция форматирования даты."""
+    return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,9 @@
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card, get_date
+
 
 print(get_mask_card_number(1234412332144123))
 
 print(get_mask_account(123456))
+
+
