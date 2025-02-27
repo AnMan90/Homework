54247bb1418d9e73816e51793e0a05b798f41886
AndreyM <kalisnrat@gmail.com>
2025-02-27 18:56:59 +0300

    start

---
+++ src/masks.py
@@ -0,0 +1,13 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Функция, принимает на вход номер карты и возвращает ее маску."""
+    card_number_str = str(card_number)
+    first_num = card_number_str[:4]
+    second_num = card_number_str[4:6]
+    last_num = card_number_str[-4:]
+    return f"{first_num} {second_num}** **** {last_num}"
+
+
+def get_mask_account(account_number: int) -> str:
+    """Функция, которая принимает на вход номер счета и возвращает его маску."""
+    account_number_str = str(account_number)
+    return f"**{account_number_str[-4:]}"
---
+++ tests/test.py
@@ -0,0 +1,5 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+print(get_mask_card_number(1234412332144123))
+
+print(get_mask_account(123456))
