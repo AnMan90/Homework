f3b853f78d1563d01d85cd996438c8dfcfe1e052
AndreyM <kalisnrat@gmail.com>
2025-03-16 19:25:18 +0300

    Package tests added test_processing.py file, which implements testing functions of processing.py module. processing.py module has also been improved. Added conftest.py fixture file.

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,20 @@
 def filter_by_state(
-    accepted_lst: list[dict[str, str | int]], filter_key: str = "EXECUTED"
+    transactions: list[dict[str, str | int]], filter_key: str = "EXECUTED"
 ) -> list[dict[str, str | int]]:
     """Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ state соответствует указанному значению."""
     filtered_by_state_lst = []
-    for dict_state in accepted_lst:
-        if dict_state["state"] == filter_key:
-            filtered_by_state_lst.append(dict_state)
+    for transaction in transactions:
+        if "state" in transaction and transaction["state"] == filter_key:
+            filtered_by_state_lst.append(transaction)
     return filtered_by_state_lst
 
 
-def sort_by_date(sortable_lst: list[dict[str, str | int]], descending: bool = True) -> list[dict[str, str | int]]:
+def sort_by_date(transactions: list[dict[str, str | int]], descending: bool = True) -> list[dict[str, str | int]]:
     """Функция должна возвращать новый список, отсортированный по дате."""
-    sortable_lst.sort(key=lambda dict_date: dict_date["date"], reverse=descending)
-    return sortable_lst
+    filtered_transactions = []
+    for item in transactions:
+        if "date" in item:
+            filtered_transactions.append(item)
+    filtered_transactions.sort(key=lambda dict_date: dict_date["date"], reverse=descending)
+    return filtered_transactions
